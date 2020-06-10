0af22a7f56bceba7486ae59b93d4f4bf91d8a251
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-06-10 22:52:48 +0200

    Display checkbox elements

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
 
-const OrderOptionCheckboxes = () => (
+import {formatPrice} from '../../../utils/formatPrice';
+
+class OrderOptionCheckboxes extends React.Component {
+  static propTypes = {
+    values: PropTypes.array,
+  }
+
+  render() {
+    const {values} = this.props;
+
+    return (
+      <div className={styles.checkboxes}>
+        {values.map( value =>(
+          <label key={value.id}>
+            <input 
+              type = 'checkbox'
+              value = {value.id}
+            />
+            {value.name}
+            {formatPrice(value.price)}
+          </label>
+        ))}
+      </div>
+    );
+  }
+}
+
+/*const OrderOptionCheckboxes = () => (
   <div>OrderOptionCheckboxes</div>
-);
+); */
 
 export default OrderOptionCheckboxes;
