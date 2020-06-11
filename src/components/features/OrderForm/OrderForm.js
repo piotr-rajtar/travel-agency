fbac467352e89995c7b74de354e485cd9c8fb9ed
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-06-11 19:22:43 +0200

    File cleaning

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -33,17 +33,4 @@ class OrderForm extends React.Component {
     }
 }
 
-/*const OrderForm = ({tripCost, options}) => (
-  <Row>
-    <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options} />
-    </Col>
-  </Row>
-);
-
-OrderForm.propTypes = {
-  tripCost: PropTypes.string,
-  options: PropTypes.object,
-}; */
-
 export default OrderForm;
