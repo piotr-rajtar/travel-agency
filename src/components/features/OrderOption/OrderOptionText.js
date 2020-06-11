7c40a6722583b1028c669c34cf66331d6229779e
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-06-11 22:36:09 +0200

    Add OrderOptionText component

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+
+const OrderOptionText = ({currentValue, setOptionValue}) => (
+  <div>
+    <input
+      className={styles.input}
+      value = {currentValue}
+      onChange = {event => setOptionValue(event.currentTarget.value)}
+    />
+  </div>
+);
+
+OrderOptionText.propTypes = {
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionText;
