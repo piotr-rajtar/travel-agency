669cd4aa31a0396e85c892cfcd89356bd99e6d73
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-06-17 18:03:23 +0200

    Add API settings

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,19 +4,60 @@ import PropTypes from 'prop-types';
 
 import {Row, Col} from 'react-flexbox-grid';
 
+import Button from '../../common/Button/Button';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import pricing from '../../../data/pricing.json';
+import settings from '../../../data/settings';
+
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+
+const sendOrder = (options, tripCost, tripData) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...tripData,
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 class OrderForm extends React.Component {
     static propTypes = {
       tripCost: PropTypes.string,
       options: PropTypes.object,
       setOrderOption: PropTypes.func,
+      tripId: PropTypes.string,
+      tripName: PropTypes.string,
+      tripCountryCode: PropTypes.string,
     }
   
     render() {
-      const {tripCost, options, setOrderOption} = this.props;
+      const {tripCost, options, setOrderOption, tripId, tripName, tripCountryCode} = this.props;
+      const tripData = {
+        tripid: tripId, 
+        tripName: tripName, 
+        countryCode: tripCountryCode.alpha3Code,
+      };
 
       return (
         <Row>
@@ -27,6 +68,9 @@ class OrderForm extends React.Component {
           ))}
           <Col xs={12}>
             <OrderSummary tripCost={tripCost} options={options} />
+          </Col>
+          <Col xs={12}>
+            <Button onClick={() => sendOrder(options, tripCost, tripData)}>Order now!</Button>
           </Col>
         </Row>
       );
